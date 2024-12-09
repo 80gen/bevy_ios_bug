6bc63d9affe19318286c62f7b150f49933990334
Wentao Zheng <zhengwt@gmail.com>
2024-12-09 15:11:40 -0800

    update to 0.14.2

--- crates/game/src/lib.rs
+++ crates/game/src/lib.rs
@@ -2,8 +2,11 @@ use bevy::prelude::*;
 
 #[cfg(any(target_os = "android", target_os = "ios"))]
 fn make_window() -> Window {
+    use bevy::window::WindowMode;
+
     Window {
         resizable: false,
+        mode: WindowMode::BorderlessFullscreen,
         recognize_rotation_gesture: true,
         ..default()
     }
@@ -30,5 +33,47 @@ pub fn run() {
             primary_window: make_window().into(),
             ..default()
         }))
+        .add_systems(Startup, setup)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // this camera renders whatever is on `PIXEL_PERFECT_LAYERS` to the canvas
+    let init = Transform::from_translation(Vec3::new(0., 0., 20.)).looking_at(Vec3::ZERO, Vec3::Y);
+    commands.spawn(Camera3dBundle {
+        camera: Camera {
+            hdr: true,
+            clear_color: ClearColorConfig::Custom(Color::BLACK),
+            ..default()
+        },
+        camera_3d: Camera3d::default(),
+        transform: init,
+        ..default()
+    });
+
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Circle::new(4.0)),
+        material: materials.add(Color::WHITE),
+        transform: Transform::from_rotation(Quat::from_rotation_x(-std::f32::consts::FRAC_PI_2)),
+        ..default()
+    });
+    commands.spawn(MaterialMeshBundle {
+        mesh: meshes.add(Cuboid::new(1.0, 1.0, 1.0)),
+        material: materials.add(Color::srgb_u8(124, 144, 255)),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
 }
