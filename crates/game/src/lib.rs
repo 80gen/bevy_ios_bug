11851b1609dfa6ad6dc5ac366af012dbdcd36d75
Wentao Zheng <zhengwt@gmail.com>
2024-12-09 12:44:42 -0800

    setup project with empty game and desktop build

---
+++ crates/game/src/lib.rs
@@ -0,0 +1,36 @@
+use bevy::prelude::*;
+
+#[cfg(any(target_os = "android", target_os = "ios"))]
+fn make_window() -> Window {
+    use bevy::window::WindowMode;
+    Window {
+        resizable: false,
+        mode: WindowMode::BorderlessFullscreen,
+        recognize_rotation_gesture: true,
+        ..default()
+    }
+}
+
+#[cfg(not(any(target_os = "android", target_os = "ios")))]
+fn make_window() -> Window {
+    use bevy::window::EnabledButtons;
+
+    Window {
+        resizable: false,
+        enabled_buttons: EnabledButtons {
+            minimize: false,
+            maximize: false,
+            ..Default::default()
+        },
+        ..default()
+    }
+}
+
+pub fn run() {
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: make_window().into(),
+            ..default()
+        }))
+        .run();
+}
